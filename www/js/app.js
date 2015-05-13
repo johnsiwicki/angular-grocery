8f00c3e0ff24d3b4f7aca1c6688a324c9679d0cc
John Siwicki <johnsiwicki@gmail.com>
2015-05-12 22:30:53 -0400

    First try at firebase app

---
+++ www/js/app.js
@@ -0,0 +1,29 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'firebase'])
+
+.factory('Items', ['$firebaseArray', function($firebaseArray) {
+  var itemsRef = new Firebase('https://grocery-ionic.firebaseio.com/items');
+  return $firebaseArray(itemsRef);
+}])
+
+ .controller('ListCtrl', function($scope, $ionicListDelegate, Items) {
+  $scope.items = Items;
+  $scope.addItem = function() {
+    var name = prompt ('What do you need to buy?')
+    if (name) {
+      $scope.items.$add({
+        'name': name
+      });
+    }
+  }
+
+  $scope.purchaseItem = function(item) {
+    var itemRef = new Firebase ('https://grocery-ionic.firebaseio.com/items')
+    itemRef.child('status').set('purchased');
+    $ionicListDelegate.closeOptionButtons();
+  };
+ });
